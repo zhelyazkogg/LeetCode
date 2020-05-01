8c9a2000140176b962714000057eee2f3238aa15
unknown <zhelyazkogg@gmail.com>
2020-05-01 17:15:24 +0300

    smth

--- src/Day1/Main.kt
+++ src/Day1/Main.kt
@@ -2,10 +2,10 @@ package Day1
 
 class Main: VersionControl() {
     override fun firstBadVersion(n: Int): Int {
-        var n = 5
-        var badVersion = 4
-        val goodVersion = arrayOf(1,2,3,4,5)
-        print(goodVersion)
+        var n = 6
+        //  var badVersion = 4
+        val goodVersion = arrayOf(0,0,0,1,1,1)
+        //
         return 0
     }
 }
