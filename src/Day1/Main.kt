5fe0d72cd451bff888df9836744cdf7b4eedcb3b
unknown <zhelyazkogg@gmail.com>
2020-05-01 19:35:56 +0300

    if done

--- src/Day1/Main.kt
+++ src/Day1/Main.kt
@@ -4,12 +4,18 @@ class Main: VersionControl() {
     override fun firstBadVersion(n: Int) : Int {
         val n = 5
         val isBadVersion = arrayOf(false, false, false, true, true)
-        for(n in isBadVersion){
-            if(n == false){
-                println("this is a good version -> " + n)
-            } else {
-                println("the bad version is -> " + n)
-            }
+        //   for(n in isBadVersion){
+        //       if(n == false){
+        //           println("this is a good version -> " + n)
+        //       } else {
+        //           println("this is a bad version  -> " + n)
+        //       }
+        //   }
+        var i = 0
+        //  val isBadVersion = arrayOf(1, 2, 3, 4, 5)
+        while(i<n){
+            ++i
+
         }
         return n
     }
