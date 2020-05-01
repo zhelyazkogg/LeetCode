1ac4edb2afe6efcae076f80e17aafab0e8f5a1d0
unknown <zhelyazkogg@gmail.com>
2020-05-01 17:05:54 +0300

    day1

---
+++ src/Day1/Main.kt
@@ -0,0 +1,11 @@
+package Day1
+
+class Main: VersionControl() {
+    override fun firstBadVersion(n: Int): Int {
+        var n = 5
+        var badVersion = 4
+        val goodVersion = arrayOf(1,2,3,4,5)
+        print(goodVersion)
+        return 0
+    }
+}
---
+++ src/Day1/VersionControl.kt
@@ -0,0 +1,8 @@
+package Day1
+
+open class VersionControl {
+
+    open fun firstBadVersion(n: Int): Int {
+        TODO("Not yet implemented")
+    }
+}
