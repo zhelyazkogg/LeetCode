82efc3ebf473015aaf73b57c582a8277a34fbaae
unknown <zhelyazkogg@gmail.com>
2020-05-01 18:09:02 +0300

    printing out the true and false

--- src/Day1/Main.kt
+++ src/Day1/Main.kt
@@ -1,11 +1,16 @@
 package Day1
 
 class Main: VersionControl() {
-    override fun firstBadVersion(n: Int): Int {
-        var n = 6
-        //  var badVersion = 4
-        val isBadVersion = arrayOf(false, false, false, true, true )
-        //
-        return 0
+    override fun firstBadVersion(n: Int) : Int {
+        val n = 5
+        val isBadVersion = arrayOf(false, false, false, true, true)
+        for(n in isBadVersion){
+            if(n == false){
+                println("this is a good version -> " + n)
+            } else {
+                println("the bad version is -> " + n)
+            }
+        }
+        return n
     }
 }
