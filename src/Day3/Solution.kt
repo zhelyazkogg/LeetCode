cd6c3474277ab8f6b3153edf6b86fb79fe76ff07
unknown <zhelyazkogg@gmail.com>
2020-05-03 19:36:44 +0300

    working on it.

--- src/Day3/Solution.kt
+++ src/Day3/Solution.kt
@@ -1,4 +1,26 @@
 package Day3
+/*
+Given an arbitrary ransom note string and another string containing letters from all the magazines,
+write a function that will return true if the ransom note can be constructed from the magazines ;
+otherwise, it will return false.
 
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+*/
 class Solution {
+    fun main(args: Array<String>) {
+        fun canConstruct(ransomNote: String, magazine: String): Boolean {
+            for((index, value) in magazine.withIndex()){
+                println(ransomNote[0]==magazine[index])
+
+            }
+            return true
+        }
+    }
 }
